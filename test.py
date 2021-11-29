0543966b50f5175f42de4cbb25678f00aee36535
Syam <syamsv2020@gmail.com>
2021-11-29 22:40:03 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+import blowfish
+import base64
+
+
+cipher = blowfish.Cipher(b"cyber_group_project")
+
+def encrypt_msg(msg):
+    msg = str.encode(msg)
+    cipher_text = b"".join(cipher.encrypt_ecb_cts(msg))
+    cipher_text = base64.b64encode(cipher_text)
+    return cipher_text.decode('utf-8')
+
+def decrypt_msg(msg):
+    msg = str.encode(msg)
+    cipher_text = base64.b64decode(msg)
+    plain_text = b"".join(cipher.decrypt_ecb_cts(cipher_text))
+    return plain_text.decode('utf-8')
+
+if __name__ == "__main__":
+    msg = "Testing Blowfish: Success \u2713"
+    val = encrypt_msg(msg)
+    print(val)
+    val = decrypt_msg(val)
+    print(val)
